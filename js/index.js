0c0dc3c5aeb585c8ca33bc33e23b612c9901bc81
mjbp <mick.perkins@stormid.com>
2018-03-09 13:57:19 +0000

    Update 2018-03-09T13:57:17.036Z

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 /**
  * @name storm-toggle: Toggle UI state accessibly
- * @version 1.2.3: Fri, 09 Mar 2018 13:43:59 GMT
+ * @version 1.2.4: Fri, 09 Mar 2018 13:56:40 GMT
  * @author stormid
  * @license MIT
  */
